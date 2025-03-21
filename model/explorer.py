62acfc5c0ddddbb4c0df241b0f26815554c446a9
Sergey Zholudev <serg.zholudev@gmail.com>
2025-03-21 15:38:55 +0300

    The project is back to the start.

--- model/explorer.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class Explorer:
-    name: str
-    country: str
-    age: int
--- web/explorer.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/explorer")
-
-
-@router.get("")
-@router.get("/")
-def get_all():
-    return "All explorers"
