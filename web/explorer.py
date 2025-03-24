6e67756d9f583084123f81befc689567487f30c5
Sergey Zholudev <serg.zholudev@gmail.com>
2025-03-24 18:22:17 +0300

    Fake data is added. We're not going to connect to the real DB yet. Model is added. Web is slightly changed.

--- web/explorer.py
+++ web/explorer.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter
+from model.explorer import Explorer
+import fake.explorer as service
 
 router = APIRouter(prefix="/explorer")
 
 
+@router.get("")
 @router.get("/")
 def top():
-    return "top here"
+    return service.get_all()
+
+
+@router.get("/{name}")
+def get_one(name):
+    return service.get_one(name)
