bf1b09ea23a395845c6f0059d138a1100d2f3462
Sergey Zholudev <serg.zholudev@gmail.com>
2025-03-25 17:19:41 +0300

    Middleware is added. CORSMiddle is added and configured.

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ from fastapi import FastAPI
 from web import explorer
 import uvicorn
 
-app = FastAPI()
+from starlette import middleware
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+]
+
+app = FastAPI(middleware=middleware)
 
 app.include_router(explorer.router)
 
